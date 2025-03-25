ba3d0ef36ce97532900c3615842a403833fe665a
Nghia Trung <nghia250803@gmail.com>
2025-03-25 17:08:06 +0700

    Update Document

--- Exception/TestClass.java
+++ Exception/TestClass.java
@@ -1,6 +1,13 @@
 package Exception;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.text.DecimalFormat;
+import java.text.Format;
+import java.text.NumberFormat;
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -8,33 +15,35 @@ import java.time.format.FormatStyle;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Deque;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
+import java.util.OptionalDouble;
 import java.util.Set;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ConcurrentSkipListSet;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.BiFunction;
+import java.util.function.BiPredicate;
+import java.util.function.DoubleFunction;
 import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.UnaryOperator;
+import java.util.stream.Collectors;
 import java.util.stream.DoubleStream;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+public class TestClass {
 
-class Game{
-  public void play() throws Exception{
-    System.out.println("Playing...");
-  }
-}
-public class TestClass extends Game{    
-  
-  public void play() {
-    System.out.println("Playing Soccer");
-  }
-  public static void main(String[] args)    {  
-String a = null;
-System.out.println(a);         
-  }
-}
+  public static void main(String[] args){
+    IntStream is = IntStream.range(1, 6);       IntStream is2 = is.takeWhile(x-> x%2==0);       is2.forEach(System.out::print);
+ }
+   }
